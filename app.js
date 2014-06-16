dbc0dc593c79d2b0ea0c82309bade23bcadb3bb6
Rebecca Cottrell <r.e.cottrell@gmail.com>
2014-06-16 15:29:05 +0100

    fixing app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var env = process.env.NODE_ENV || 'development';
 console.log(username + ', ' + password);
 
 // Application settings
- 
+
 if (env == 'production'){
 	if (!username || !password){
 		console.log('Username or password is not set, exiting.');
@@ -18,7 +18,7 @@ if (env == 'production'){
 	}
 	app.use(express.basicAuth(username, password));
 }
-  
+
 // Application settings
 app.engine('html', cons.mustache);
 app.set('view engine', 'html');
@@ -44,104 +44,61 @@ app.get('/', function (req, res) {
 
   res.render('index', {
             'pageTitle': 'Digital Marketplace',
-            'head' : head 
+            'head' : head
             });
-  
+
 });
 
 app.get('/sample', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('sample', {
             'pageTitle': 'sample',
-            'head' : head 
+            'head' : head
             });
 });
 
 app.get('/search_results.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('search_results.html', {
             'pageTitle': 'Search results',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/search_results_2.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('search_results_2.html', {
             'pageTitle': 'Search results',
-            'head' : head 
-            });
-});
-
-app.get('/search_results_3.html', function (req, res) {
-  
-  var head = commonHead;
-
-  res.render('search_results_3.html', {
-            'pageTitle': 'Search results',
-            'head' : head 
-            });
-});
-
-
-
-app.get('/user-account-confirmation.html', function (req, res) {
-  
-  var head = commonHead;
-
-  res.render('user-account-confirmation.html', {
-            'pageTitle': 'User account confirmation',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/bad-link.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('bad-link.html', {
             'pageTitle': 'Invalid link',
-            'head' : head 
-            });
-});
-
-app.get('/admin-console.html', function (req, res) {
-  
-  var head = commonHead;
-
-  res.render('admin-console.html', {
-            'pageTitle': 'Admin console',
-            'head' : head 
-            });
-});
-
-
-app.get('/admin-create-user.html', function (req, res) {
-  
-  var head = commonHead;
-
-  res.render('admin-create-user.html', {
-            'pageTitle': 'Create user',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/supplier.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('supplier.html', {
             'pageTitle': 'Supplier',
-            'head' : head 
+            'head' : head
             });
 });
 
@@ -149,98 +106,98 @@ app.get('/supplier.html', function (req, res) {
 
 
 app.get('/listing.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('listing.html', {
             'pageTitle': 'Listing page',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/supplier-dashboard.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('supplier-dashboard.html', {
             'pageTitle': 'Supplier dashboard',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/buyer-dashboard.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('buyer-dashboard.html', {
             'pageTitle': 'Buyer dashboard',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 
 app.get('/add-project.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('add-project.html', {
             'pageTitle': 'Add a new project',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/listing-no-photo.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('listing-no-photo.html', {
             'pageTitle': 'Listing',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/add-project-example.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('add-project-example.html', {
             'pageTitle': 'Add project',
-            'head' : head 
+            'head' : head
             });
 });
 
 app.get('/add-project-with-compare.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('add-project-with-compare.html', {
             'pageTitle': 'Add project',
-            'head' : head 
+            'head' : head
             });
 });
 
 app.get('/login.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('login.html', {
             'pageTitle': 'Login to Digital Marketplace',
-            'head' : head 
+            'head' : head
             });
 });
 
 app.get('/sign-up.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('sign-up.html', {
             'pageTitle': 'Sign up to Digital Marketplace',
-            'head' : head 
+            'head' : head
             });
 });
 
@@ -248,123 +205,133 @@ app.get('/sign-up.html', function (req, res) {
 
 
 app.get('/supplier-dashboard.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('supplier-dashboard.html', {
             'pageTitle': 'Supplier dashboard',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/shortlist.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('shortlist.html', {
             'pageTitle': 'Shortlist',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/saas.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('saas.html', {
             'pageTitle': 'Software as a service',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/savedforlater.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('savedforlater.html', {
             'pageTitle': 'Saved for later',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 
 app.get('/share.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('share.html', {
             'pageTitle': 'Share',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/reset.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('reset.html', {
             'pageTitle': 'Reset password',
-            'head' : head 
+            'head' : head
             });
 });
 
 app.get('/successfully-reset.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('successfully-reset.html', {
             'pageTitle': 'Your password has been reset',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 app.get('/buyer-dashboard-pw-changed.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('buyer-dashboard-pw-changed.html', {
             'pageTitle': 'Buyer dashboard',
-            'head' : head 
+            'head' : head
             });
 });
 
 app.get('/all-services.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('all-services.html', {
             'pageTitle': 'All services',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 
 app.get('/govuk_landing.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('govuk_landing.html', {
             'pageTitle': 'Buy and supply digital services',
-            'head' : head 
+            'head' : head
             });
 });
 
 
 
 app.get('/govuk_landing2.html', function (req, res) {
-  
+
   var head = commonHead;
 
   res.render('govuk_landing2.html', {
             'pageTitle': 'Buy and supply digital services',
-            'head' : head 
+            'head' : head
+            });
+});
+
+app.get('/searchbuilder', function (req, res) {
+
+  var head = commonHead;
+
+  res.render('search-builder.html', {
+            'pageTitle': 'Buy and supply digital services',
+            'head' : head
             });
 });
 
