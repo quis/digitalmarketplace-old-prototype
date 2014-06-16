6e8f07b322c295a620c44a1ccc565bc0f8d13157
Rebecca Cottrell <r.e.cottrell@gmail.com>
2014-06-16 15:44:49 +0100

    adding config.js

--- govuk/config.js
+++ govuk/config.js
@@ -18,5 +18,5 @@ exports.govukConfig = {
   'afterHeader': '<div id="phase-notice">' +
                     '<p><strong>Alpha:</strong> This is a new service. To help us improve it please send feedback to <a href="mailto:digitalmarketplace@digital.cabinet-office.gov.uk">digitalmarketplace@digital.cabinet-office.gov.uk</a></p>' +
                   '</div>',
-  'bodyEnd': '<script src="/public/javascripts/application.js" type="text/javascript"></script>'
+  'bodyEnd': '<script src="/public/javascripts/application.js" type="text/javascript"></script><script src="/public/javascripts/searchBuilder.js" type="text/javascript"></script>'
 };
